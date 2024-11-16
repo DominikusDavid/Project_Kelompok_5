a9932e1f1f2c274f72710d3806a25670d3c8240f
DominikusDavid <152621597+DominikusDavid@users.noreply.github.com>
2024-11-16 23:54:32 +0700

    Add files via upload

--- ProjectAwal.py
+++ ProjectAwal.py
@@ -3,7 +3,64 @@ Nama = str(input("Masukkan ID Anda : "))
 IDCard = int(input("Masukkan Nomor Anda : "))
 
 print(f"Welcome back {Nama}")
-if "#" in Nama: pass
+if "#" in Nama: 
+    #Dashboard Buyer
+    JumlahUang = 0
+    KodePerusahaan = []
+    sumLembarSaham = []
+    HargaSaham = 1 #Asumsi Harga saham semua perusahaan = 1 karena disini tidak ada data harga saham
+    LembarSaham = 0
+
+    Dashboard = str("Dashboard Buyer\n"
+                    "Pilih salah satu\n"
+                    "1. Topup\n"
+                    "2. Pembelian\n"
+                    "3. Cancel")
+    
+    print(Dashboard)
+    InputBuyer = str(input())   
+
+    while(InputBuyer != "3"):
+        if(InputBuyer == "1"):
+            JumlahPembelian = int(input("Input Jumlah Topup: "))
+            while(JumlahPembelian<10000000):
+                print("Jumlah Invalid! Topup tidak bisa kurang dari 10 juta")
+                JumlahPembelian = int(input("Input Jumlah Topup: "))
+            JumlahUang += JumlahPembelian
+        
+            print(f"Saldo Akhir: {JumlahUang}")
+            print(Dashboard)
+            InputBuyer = str(input())
+    
+        elif(InputBuyer == "2"):
+            print(f"Saldo: {JumlahUang}")
+            Kode = str(input("Input Kode Perusahaan: "))
+        
+            if(Kode in KodePerusahaan):
+                for i in range(len(KodePerusahaan)):
+                    if(Kode == KodePerusahaan[i]):
+                        JumlahPembelian = int(input("Jumlah Pembelian: "))
+                        while(JumlahPembelian>JumlahUang):
+                            print("Jumlah Pembelian Invalid!")
+                            JumlahPembelian = int(input("Jumlah Pembelian: "))
+                        LembarSaham = JumlahPembelian/HargaSaham
+                        sumLembarSaham[i] = sumLembarSaham[i] + LembarSaham
+        
+            elif(Kode not in KodePerusahaan):
+                KodePerusahaan.append(Kode)
+                JumlahPembelian = int(input("Jumlah Pembelian: "))
+                while(JumlahPembelian>JumlahUang):
+                    print("Jumlah Pembelian Invalid!")
+                    JumlahPembelian = int(input("Jumlah Pembelian: "))
+                LembarSaham = JumlahPembelian/HargaSaham
+                sumLembarSaham.append(LembarSaham)
+            JumlahUang -= JumlahPembelian
+            print(f"Sisa Uang: {JumlahUang}\n"
+                f"Saham yang dipunya: {KodePerusahaan}\n"
+                f"Jumlah lembar saham yang dipunya: {sumLembarSaham}\n")
+            print(f"\n{Dashboard}")
+            InputBuyer = str(input())
+            
 elif "*" in Nama : pass
 elif "%" in Nama :  
 
@@ -13,13 +70,13 @@ elif "%" in Nama :
 
     while i < jumlahPerusahaan:
 
-        kodePerusahaan = str(input(""))
+        KodePerusahaan = str(input(""))
 
-        if "$" in kodePerusahaan:
-            kodePerusahaanArr.append(kodePerusahaan)
+        if "$" in KodePerusahaan:
+            kodePerusahaanArr.append(KodePerusahaan)
             i += 1
 
-        elif "$" in kodePerusahaan:
+        elif "$" in KodePerusahaan:
             print("Masukkan kodePerusahaan dengan awalan $")
 
         else : pass
@@ -40,5 +97,62 @@ elif "%" in Nama :
         
         volumeIPO = int(input("Masukkan jumlah lembar saham IPO: "))
         volumeIPOArr.append(volumeIPO)
+
+    #Dashboard Buyer
+    JumlahUang = 0
+    KodePerusahaan = []
+    sumLembarSaham = []
+    HargaSaham = 1 #Asumsi Harga saham semua perusahaan = 1 karena disini tidak ada data harga saham
+    LembarSaham = 0
+
+    Dashboard = str("Dashboard Buyer\n"
+                    "Pilih salah satu\n"
+                    "1. Topup\n"
+                    "2. Pembelian\n"
+                    "3. Cancel")
+    
+    print(Dashboard)
+    InputBuyer = str(input())   
+
+    while(InputBuyer != "3"):
+        if(InputBuyer == "1"):
+            JumlahPembelian = int(input("Input Jumlah Topup: "))
+            while(JumlahPembelian<10000000):
+                print("Jumlah Invalid! Topup tidak bisa kurang dari 10 juta")
+                JumlahPembelian = int(input("Input Jumlah Topup: "))
+            JumlahUang += JumlahPembelian
         
+            print(f"Saldo Akhir: {JumlahUang}")
+            print(Dashboard)
+            InputBuyer = str(input())
+    
+        elif(InputBuyer == "2"):
+            print(f"Saldo: {JumlahUang}")
+            Kode = str(input("Input Kode Perusahaan: "))
+        
+            if(Kode in KodePerusahaan):
+                for i in range(len(KodePerusahaan)):
+                    if(Kode == KodePerusahaan[i]):
+                        JumlahPembelian = int(input("Jumlah Pembelian: "))
+                        while(JumlahPembelian>JumlahUang):
+                            print("Jumlah Pembelian Invalid!")
+                            JumlahPembelian = int(input("Jumlah Pembelian: "))
+                        LembarSaham = JumlahPembelian/HargaSaham
+                        sumLembarSaham[i] = sumLembarSaham[i] + LembarSaham
+        
+            elif(Kode not in KodePerusahaan):
+                KodePerusahaan.append(Kode)
+                JumlahPembelian = int(input("Jumlah Pembelian: "))
+                while(JumlahPembelian>JumlahUang):
+                    print("Jumlah Pembelian Invalid!")
+                    JumlahPembelian = int(input("Jumlah Pembelian: "))
+                LembarSaham = JumlahPembelian/HargaSaham
+                sumLembarSaham.append(LembarSaham)
+            JumlahUang -= JumlahPembelian
+            print(f"Sisa Uang: {JumlahUang}\n"
+                f"Saham yang dipunya: {KodePerusahaan}\n"
+                f"Jumlah lembar saham yang dipunya: {sumLembarSaham}\n")
+            print(f"\n{Dashboard}")
+            InputBuyer = str(input())
+                    
 else : pass
