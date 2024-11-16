33afa796e68f0c1d33630dc6d01d1fb2013201f3
DominikusDavid <152621597+DominikusDavid@users.noreply.github.com>
2024-11-17 00:35:36 +0700

    Add files via upload

--- ProjectAwal.py
+++ ProjectAwal.py
@@ -34,32 +34,7 @@ if "#" in Nama:
     
         elif(InputBuyer == "2"):
             print(f"Saldo: {JumlahUang}")
-            Kode = str(input("Input Kode Perusahaan: "))
-        
-            if(Kode in KodePerusahaan):
-                for i in range(len(KodePerusahaan)):
-                    if(Kode == KodePerusahaan[i]):
-                        JumlahPembelian = int(input("Jumlah Pembelian: "))
-                        while(JumlahPembelian>JumlahUang):
-                            print("Jumlah Pembelian Invalid!")
-                            JumlahPembelian = int(input("Jumlah Pembelian: "))
-                        LembarSaham = JumlahPembelian/HargaSaham
-                        sumLembarSaham[i] = sumLembarSaham[i] + LembarSaham
-        
-            elif(Kode not in KodePerusahaan):
-                KodePerusahaan.append(Kode)
-                JumlahPembelian = int(input("Jumlah Pembelian: "))
-                while(JumlahPembelian>JumlahUang):
-                    print("Jumlah Pembelian Invalid!")
-                    JumlahPembelian = int(input("Jumlah Pembelian: "))
-                LembarSaham = JumlahPembelian/HargaSaham
-                sumLembarSaham.append(LembarSaham)
-            JumlahUang -= JumlahPembelian
-            print(f"Sisa Uang: {JumlahUang}\n"
-                f"Saham yang dipunya: {KodePerusahaan}\n"
-                f"Jumlah lembar saham yang dipunya: {sumLembarSaham}\n")
-            print(f"\n{Dashboard}")
-            InputBuyer = str(input())
+            print("Tidak tersedia perusahaan IPO")
             
 elif "*" in Nama : pass
 elif "%" in Nama :  
@@ -100,7 +75,6 @@ elif "%" in Nama :
 
     #Dashboard Buyer
     JumlahUang = 0
-    KodePerusahaan = []
     sumLembarSaham = []
     HargaSaham = 1 #Asumsi Harga saham semua perusahaan = 1 karena disini tidak ada data harga saham
     LembarSaham = 0
@@ -141,13 +115,8 @@ elif "%" in Nama :
                         sumLembarSaham[i] = sumLembarSaham[i] + LembarSaham
         
             elif(Kode not in KodePerusahaan):
-                KodePerusahaan.append(Kode)
-                JumlahPembelian = int(input("Jumlah Pembelian: "))
-                while(JumlahPembelian>JumlahUang):
-                    print("Jumlah Pembelian Invalid!")
-                    JumlahPembelian = int(input("Jumlah Pembelian: "))
-                LembarSaham = JumlahPembelian/HargaSaham
-                sumLembarSaham.append(LembarSaham)
+                print("Masukkan kode perushaan yang tersedia")
+                
             JumlahUang -= JumlahPembelian
             print(f"Sisa Uang: {JumlahUang}\n"
                 f"Saham yang dipunya: {KodePerusahaan}\n"
